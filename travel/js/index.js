cbb053aace8205455c968236f42b69d4ea51ae12
RyanL <a11160725@gmail.com>
2023-05-25 19:44:24 +0800

    update travel

--- travel/js/index.js
+++ travel/js/index.js
@@ -1,151 +1,192 @@
-// 讀取資料
-let requestUrl =
-  "https://raw.githubusercontent.com/hexschool/KCGTravel/master/datastore_search.json";
+// 最新資料
+// const xhr = new XMLHttpRequest();
+// const jsonURL = 'https://api.kcg.gov.tw/api/service/get/9c8e1450-e833-499c-8320-29b36b7ace5c';
+// xhr.open('get', jsonURL, false);
+// xhr.send();
+// const jsonData = JSON.parse(xhr.responseText);
+// const allData = jsonData.data.XML_Head.Infos.Info;
+
+// const allDataLen = allData.length;
+
 const xhr = new XMLHttpRequest();
-xhr.open("get", requestUrl, false);
+const jsonURL = 'https://raw.githubusercontent.com/hexschool/KCGTravel/master/datastore_search.json';
+xhr.open('get', jsonURL, false);
 xhr.send();
-const data = JSON.parse(xhr.response).result.records;
+const jsonData = JSON.parse(xhr.response);
+const allData = jsonData.result.records;
+const allDataLen = allData.length; 
 
-// 指定DOM元素
-const menu = document.querySelector(".menu");
-const Zuoying = document.querySelector(".Zuoying");
-const content = document.querySelector(".content");
-const switchPageBtn = document.querySelector(".switchPageBtn");
-
-// 監聽事件
-menu.addEventListener("change", updateList);
-switchPageBtn.addEventListener("click", switchPage);
+let tempData = [];
+let currentPageData = [];
+let currentPage = 1;
+let perPage = 6;
 
 // 預設畫面
-pagination(data, 1);
-
-// 下拉式選單
-let zone = [];
-for (let i = 0; i < data.length; i++) {
-  zone.push(data[i].Zone);
-}
-let uniqueZone = [...new Set(zone)];
-let uniqueZoneLen = uniqueZone.length;
-let zoneTxt = `<option value="全部行政區" selected> - - 全部行政區 - - </option>`;
-for (let i = 0; i < uniqueZoneLen; i++) {
-  zoneTxt += `<option value="${uniqueZone[i]}">${uniqueZone[i]}</option>`;
-}
-menu.innerHTML = zoneTxt;
-
-// 頁面更新
-function updateList(e) {
-  let select = e.target.value;
-  // console.log(select);
-  let attractionsTxt = `<span class="districtTitle">${select}</span>`;
-  for (let i = 0; i < data.length; i++) {
-    if (select == data[i].Zone) {
-      attractionsTxt += `
-        <ul class="attractions">
-          <li><img src="${data[i].Picture1}" class="attractionsPicture">
-          </li>
-          <li><span class="attractionsText"><img src="./img/icons_clock.png" class="attractionsIcons">${data[i].Opentime}</span></li>
-          <li><span class="attractionsText"><img src="./img/icons_pin.png" class="attractionsIcons">${data[i].Add}</span></li>
-          <li><span class="attractionsText"><img src="./img/icons_phone.png" class="attractionsIcons">${data[i].Tel}</span></li>
-          <li class="attractionsName">${data[i].Name}</li>
-          <li class="attractionsZone">${data[i].Zone}</li>
-        </ul>
-      `;
-    } else if (select == "全部行政區") {
-      attractionsTxt += `
-        <ul class="attractions">
-          <li><img src="${data[i].Picture1}" class="attractionsPicture">
-          </li>
-          <li><span class="attractionsText"><img src="./img/icons_clock.png" class="attractionsIcons">${data[i].Opentime}</span></li>
-          <li><span class="attractionsText"><img src="./img/icons_pin.png" class="attractionsIcons">${data[i].Add}</span></li>
-          <li><span class="attractionsText"><img src="./img/icons_phone.png" class="attractionsIcons">${data[i].Tel}</span></li>
-          <li class="attractionsName">${data[i].Name}</li>
-          <li class="attractionsZone">${data[i].Zone}</li>
-        </ul>
-      `;
-    }
-  }
-  content.innerHTML = attractionsTxt;
+for (let i = 0; i < allDataLen; i++) {
+  tempData.push(allData[i]);
 }
 
-// 分頁功能
-function pagination(data, nowPage) {
-  const dataTotal = data.length;
-  const perpage = 6;
-  // pageTotal 按鈕總數量 = 總資料數量 / 每一頁要顯示的資料 (因為有可能會出現餘數，所以要無條件進位。)
-  const pageTotal = Math.ceil(dataTotal / perpage);
-  let currentPage = nowPage;
-  // 每頁第一筆資料
-  const minData = currentPage * perpage - perpage + 1;
-  // 每頁最後一筆資料
-  const maxData = currentPage * perpage;
+// 指定 DOM 元素
+const selector = document.querySelector('.selector');
+const popularList = document.querySelector('.popularList');
+const districtTitle = document.querySelector('.districtTitle');
+const districtList = document.querySelector('.districtList');
+const pageList = document.querySelector('.pageList');
 
-  let currentData = [];
-  for (let i = minData - 1; i < maxData; i++) {
-    currentData.push(data[i]);
-  }
+// 監聽事件
+selector.addEventListener('change', selectorList);
+popularList.addEventListener('click', UpdatePopList);
+pageList.addEventListener('click', switchPage);
 
-  const page = {
-    pageTotal,
-    currentPage,
-    hasPrevBtn: currentPage > 1,
-    hasNextBtn: currentPage < pageTotal,
+
+renderSelector();
+displayData();
+renderList(currentPageData);
+renderPageList(tempData);
+
+function renderList(array) {
+  let str = '';
+  for (let i = 0; i < array.length; i++) {
+    str +=
+    `<li class="attraction">
+      <div>
+        <img src="${array[i].Picture1}" alt="" class="attractionPicture">
+      </div>
+      <div class="attractionText">
+        <img src="./img/icons_clock.png" alt="" class="attractionIcon">
+        <p>${array[i].Opentime}</p>
+      </div>
+      <div class="attractionText">
+        <img src="./img/icons_pin.png" alt="" class="attractionIcon">
+        <p>${array[i].Add}</p>
+      </div>
+      <div class="attractionText">
+        <img src="./img/icons_phone.png" alt="" class="attractionIcon">
+        <p>${array[i].Tel}</p>
+      </div>
+      <div class="attractionName">
+        <p>${array[i].Name}</p>
+      </div>
+      <div class="attractionZone">
+        <p>${array[i].Zone}</p>
+      </div>
+    </li>`;
   };
-  displayData(currentData);
-  pageBtn(page);
+  districtList.innerHTML = str;
 }
 
-function displayData(currentData) {
-  let contentText = `<span class="districtTitle">全部行政區</span>`;
-  for (let i = 0; i < currentData.length; i++) {
-    contentText += `
-      <ul class="attractions">
-          <li><img src="${currentData[i].Picture1}" class="attractionsPicture">
-          </li>
-          <li><span class="attractionsText"><img src="./img/icons_clock.png" class="attractionsIcons">${currentData[i].Opentime}</span></li>
-          <li><span class="attractionsText"><img src="./img/icons_pin.png" class="attractionsIcons">${currentData[i].Add}</span></li>
-          <li><span class="attractionsText"><img src="./img/icons_phone.png" class="attractionsIcons">${currentData[i].Tel}</span></li>
-          <li class="attractionsName">${currentData[i].Name}</li>
-          <li class="attractionsZone">${currentData[i].Zone}</li>
-      </ul>
-    `;
-  }
-  content.innerHTML = contentText;
+function renderSelector() {
+  let districtList = [];
+  for (let i = 0; i < allDataLen; i++) {
+    districtList.push(allData[i].Zone);
+  };
+
+  let zone = ['全部行政區'];
+  zone.push(... new Set(districtList));
+
+  let str = '';
+  for (let i = 0; i < zone.length; i++) {
+    str += `<option value = "${zone[i]}">${zone[i]}</option>`;
+  };
+  selector.innerHTML = str;
 }
 
-function pageBtn(page) {
-  let str = "";
-  let total = page.pageTotal;
-  // prev
-  if (page.hasPrevBtn) {
-    str += `<li><a href="#" class="pageBtn prev" data-page="${
-      page.currentPage - 1
-    }">Prev</a></li>`;
+function UpdatePopList(e) {
+  let select = e.target.dataset.zone;
+  if (e.target.nodeName != 'A') return;
+  tempData.splice(0, tempData.length);
+  districtTitle.innerHTML = select;
+  for (let i = 0; i < allDataLen; i++) {
+    if (select == allData[i].Zone) {
+      tempData.push(allData[i]);
+    };
+  };
+  currentPage = 1;
+  displayData();
+  renderList(currentPageData);
+  renderPageList(tempData);
+}
+
+function selectorList(e) {
+  let select = e.target.value;
+  districtTitle.innerHTML = select;
+  tempData.splice(0, tempData.length);
+  if (select == "全部行政區") {
+    for (let i = 0; i < allDataLen; i++) {
+      tempData.push(allData[i]);
+    }
   } else {
-    str += `<li><span class="prev disabled">Prev</span></li>`;
+    for (let i = 0; i < allDataLen; i++) {
+      if (select == allData[i].Zone) {
+        tempData.push(allData[i]);
+      };
+    };
   }
-  // page
-  for (let i = 1; i < total; i++) {
-    if (Number(page.currentPage) == i) {
-      str += `<li><a href="#" class="pageBtn current" data-page="${i}">${i}</a></li>`;
-    } else {
-      str += `<li><a href="#" class="pageBtn" data-page="${i}">${i}</a></li>`;
+  currentPage = 1;
+  displayData();
+  renderList(currentPageData);
+  renderPageList(tempData);
+}
+
+function renderPageList(data) {
+  let dataLen = data.length;
+  let pageTotal = Math.ceil(dataLen/perPage);
+  let pre = '';
+  let str = '';
+  let nxt = '';
+  if (currentPage <= 1) {
+    pre += `<li><span class="prev disabled">Prev</span></li>`;
+  } else {
+    pre += 
+    `<li><a href="#" class="pageBtn prev" data-page="${currentPage - 1}">Prev</a></li>`;
+  };
+
+  if (currentPage >= pageTotal) {
+    nxt += `<li><span class="next disabled">Next</span></li>`;
+  } else {
+    nxt += 
+    `<li><a href="#" class="pageBtn next" data-page="${currentPage + 1}">Next</a></li>`;
+  };
+
+  if (pageTotal <= 10 && pageTotal > 1) {
+    for(let i = 1; i <= pageTotal; i++) {
+      if (Number(currentPage) == i) {
+        str += `<li><a href="#" class="pageBtn current" data-page="${i}">${i}</a></li>`;
+      } else {
+        str += `<li><a href="#" class="pageBtn" data-page="${i}">${i}</a></li>`;
+      }
+    };
+    pageList.innerHTML = 
+    `${pre}${str}${nxt}`;
+  } else {
+    pageList.innerHTML = '';
+  };
+}
+
+function displayData() {
+  currentPageData.splice(0, currentPageData.length);
+  let minData = (currentPage - 1) * perPage;
+  let maxData = (currentPage * perPage) -1;
+  let dataLen = tempData.length;
+  let pageTotal = Math.ceil(dataLen/perPage);
+  if (currentPage == pageTotal) {
+    for (let i = minData; i < dataLen; i++) {
+      currentPageData.push(tempData[i]);
+    }
+  } else {
+    for (let i = minData; i <= maxData; i++) {
+      currentPageData.push(tempData[i]);
     }
   }
-  // next
-  if (page.hasNextBtn) {
-    str += `<li><a href="#" class="pageBtn next" data-page="${
-      page.currentPage + 1
-    }">Next</a></li>`;
-  } else {
-    str += `<li><span class="next disabled">Next</span></li>`;
-  }
-  switchPageBtn.innerHTML = str;
+  console.log(currentPageData);
 }
 
 function switchPage(e) {
   e.preventDefault();
   if (e.target.nodeName !== "A") return;
-  const page = e.target.dataset.page;
-  pagination(data, page);
-  updateList();
+  let page = e.target.dataset.page;
+  currentPage = page;
+  displayData();
+  renderList(currentPageData);
+  renderPageList(tempData);
 }
